7dfc5976b806137790ec4ea29a325b2a543e2acf
Tony Russo <tony@tortugacoders.com>
2019-06-21 16:26:35 +0700

    reload products on sidebar click

--- client/js/script.js
+++ client/js/script.js
@@ -12,7 +12,7 @@ window.onload = () => {
 		categories.forEach((c) => {
 			ul.insertAdjacentHTML('beforeEnd', `
 				<li>
-					<a href="#">${c.name}</a>
+					<a href="#" class="category" id="${c.id}">${c.name}</a>
 				</li>
 			`)
 		})
@@ -48,6 +48,44 @@ window.onload = () => {
 		})
 	}).catch((err) => {
 		console.log('err', err)
+	})
+
+	// ::: Click on Category :::
+
+	document.addEventListener('click', (e) => {
+		if (e.target.classList.contains('category')) {
+			axios.get(`http://localhost:3000/api/products/${e.target.id}`).then((res) => {
+				console.log('res', res.data)
+				let products = res.data
+				// target products
+				let products_ui = document.getElementById('products')
+
+				// clear the products
+				products_ui.innerHTML = ''
+				// display each product in the DOM
+				products.forEach((p) => {
+					products_ui.insertAdjacentHTML('beforeEnd', `
+					<div class="product">
+						<div class="product-image" style="background-image: url('../img/${p.image}')">
+							<i class="far fa-star"></i>
+						</div>
+						<div class="product-extras">
+							<div class="description">
+								<h4>${p.name}</h4>
+								<small>${p.description}</small>
+							</div>
+							<div class="price">
+								<span>$${p.price}</span>
+								<a href="#" class="button">shop now</a>
+							</div>
+						</div>
+					</div>
+					`)
+				})
+			}).catch((err) => {
+				console.log('err', err)
+			})
+		}
 	})
 
 }
